8f764920d6e4afba26e50fcdf660a14160020a19
570189458 <570189458@qq.com>
2020-06-18 19:46:37 +0800

    new

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mysql_dev: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'exam-system',
+        connectionLimit: 10,
+        supportBigNumbers: true
+    }
+};
