6721d3551306e556d537ced9ca3f03a09341a78f
570189458 <570189458@qq.com>
2020-06-26 20:27:05 +0800

    吼吼吼

--- route/Student.js
+++ route/Student.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Student = express.Router()
+const Res = require('../dao/Result_dao')
 
 Student.get('/addExam', require('./Student/addExam'))
 
@@ -10,5 +11,14 @@ Student.get('/printExamInfo', require('./Student/printExamInfo'))
 Student.get('/getFileExamInfo', require('./Student/getFileExamInfo'))
 
 Student.get('/findscore', require('./Student/findsocre'))
+
+Student.get('/ans', (req, res)=>{
+    const { score } = req.query
+    const result = new Res()
+    console.log(score)
+    result.Result_save(1, req.session.userId, score, (err, docs)=>{
+        res.redirect('/student_welcome(login_success).html')
+    })
+})
 
 module.exports = Student
